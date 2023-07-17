2c10fbced7006552645f237a703a2bc15afaa6d3
Valeriy <dobrotskiyvd@gmail.com>
2023-07-17 22:08:22 +0300

    Food items stored inside bucket with physics applied

--- Assets/Scripts/Bucket/FoodBucket.cs
+++ Assets/Scripts/Bucket/FoodBucket.cs
@@ -7,6 +7,7 @@ public class FoodBucket : MonoBehaviourSingleton<FoodBucket>
     public static event Action ItemStored;
 
     [SerializeField] private List<Transform> _foodPlacementPoints;
+    [SerializeField] private GameObject _invisibleLid;
     private int _itemsInside = 0;
     private GameObject _itemToStore;
 
@@ -17,11 +18,11 @@ public class FoodBucket : MonoBehaviourSingleton<FoodBucket>
         if (_itemsInside >= _foodPlacementPoints.Count)
             return;
 
-        DisableRigidbody();
         _itemToStore.transform.SetParent(_foodPlacementPoints[_itemsInside]);
         _itemToStore.transform.localPosition = Vector3.zero;
         _itemToStore.transform.localRotation = Quaternion.identity;
         _itemToStore.transform.localScale /= 2;
+        Physics.IgnoreCollision(_invisibleLid.GetComponent<Collider>(), _itemToStore.GetComponent<Collider>(), false);
         _itemsInside++;
         _itemToStore = null;
         ItemStored?.Invoke();
